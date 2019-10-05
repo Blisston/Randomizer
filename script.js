5c131540f0f966d90d0934733f341ef704446c0e
Blisston kirubha <blisstonkirubha@gmail.com>
2019-10-06 00:18:25 +0530

    initial

---
+++ script.js
@@ -0,0 +1,215 @@
+var arr = [];
+let details;
+var openFile = function(event) {
+  var input = event.target;
+
+  var reader = new FileReader();
+  reader.onload = function() {
+    details = reader.result;
+  };
+  reader.readAsText(input.files[0]);
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll("select");
+  var instances = M.FormSelect.init(elems);
+});
+$("#chooseFile").bind("change", function() {
+  var filename = $("#chooseFile").val();
+  if (/^\s*$/.test(filename)) {
+    $(".file-upload").removeClass("active");
+    $("#noFile").text("No file chosen...");
+  } else {
+    $(".file-upload").addClass("active");
+    $("#noFile").text(filename.replace("C:\\fakepath\\", ""));
+  }
+});
+function submit() {
+  let type = document.getElementById("type").value;
+  let unit = document.getElementById("unit").value;
+  let level = document.getElementById("level").value;
+  if (type !== "" && unit !== "") {
+    initExt();
+    var d = document.getElementById("submit");
+    d.className += "modal-close";
+  } else {
+    alert("Enter Details");
+  }
+}
+function initExt() {
+  extract(5, 1);
+  extract(10, 5);
+  extract("end", 10);
+}
+function extract(index, typeQ) {
+  let type = ""+typeQ;
+  let unit = document.getElementById("unit").value;
+  let level = document.getElementById("level").value;
+  let mark = details.indexOf(index + " Mark");
+  let border = details.indexOf(typeQ + " Mark");
+  console.log(border);
+  if (mark === -1) {
+    mark = details.length;
+  }
+  let start = details.indexOf("1. ", border);
+
+  for (let i = 0; i < mark; i++) {
+    let end = details.indexOf(i + 2 + ". ", border);
+    let strend = details.indexOf("endofquestions", border);
+    if (end > strend) {
+      end = strend;
+    }
+    if (end === -1) {
+      end = strend;
+    }
+    let sentence = details.substring(start, end);
+    if (sentence.trim().length > 1) {
+      arr.push({
+        question: sentence.trim(),
+        type: type,
+        unit: unit,
+        level: level
+      });
+      start = end;
+    }
+  }
+  console.log(arr);
+}
+
+let units = [];
+function calcUnits() {
+  units = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (units.indexOf(arr[i].unit) === -1) {
+      units.push(arr[i].unit);
+    }
+  }
+  console.log(units);
+  let ux = "";
+  for (let i = 0; i < units.length; i++) {
+    let str = `<p>
+      <label>
+        <input value=${units[i]} class="selUnit" type="checkbox" />
+        <span>${units[i]}</span>
+      </label>
+    </p>`;
+    ux = ux + str;
+  }
+  console.log(ux);
+  document.getElementById("units").insertAdjacentHTML("beforeend", ux);
+}
+let questions = [];
+function generate() {
+  let no = document.getElementById("no").value;
+  let type = document.getElementById("seltype").value;
+  var checkedValue = [];
+  var inputElements = document.getElementsByClassName("selUnit");
+  for (var i = 0; inputElements[i]; ++i) {
+    if (inputElements[i].checked) {
+      checkedValue.push(inputElements[i].value);
+    }
+  }
+  console.log(seltype);
+  questionsPerUnit = no / units.length;
+  questions = [];
+  diff = 0;
+  unit = 0;
+  possibleQuestions = [];
+  if(type=='all') {
+    for (let i = 0; i < arr.length; i++) {
+        if (checkedValue.indexOf("" + arr[i].unit) !== -1) {
+          possibleQuestions.push(arr[i]);
+        }
+      }
+  }
+  else {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].type === type && checkedValue.indexOf("" + arr[i].unit) !== -1) {
+      possibleQuestions.push(arr[i]);
+    }
+  }
+  }
+
+
+  console.log(possibleQuestions);
+
+  for (let i = 0; i < units.length; i++) {
+    let unitQuestions = getUnitQuestion(
+      possibleQuestions,
+      units[unit]
+    );
+    console.log(unitQuestions);
+    for (let k = 0; k < questionsPerUnit; k++) {
+      let diffQuestion;
+      for (let j = 0; j < 3; j++) {
+        diffQuestion = getDiffQuestion(
+          unitQuestions,
+          (diff % 3) + 1,
+          questions
+        );
+        diff++;
+        if (diffQuestion !== undefined) {
+          break;
+        }
+      }
+      if(diffQuestion !== undefined) {
+      questions.push(diffQuestion);
+      }
+    }
+    unit++;
+  }
+  console.log(questions);
+  genOutput();
+}
+function getUnitQuestion(possibleQuestions, unit) {
+  unitQuestions = [];
+  console.log(unit);
+  for (let i = 0; i < possibleQuestions.length; i++) {
+    if (possibleQuestions[i].unit == unit) {
+      unitQuestions.push(possibleQuestions[i]);
+    }
+  }
+  return unitQuestions;
+}
+function getDiffQuestion(unitQuestions, diff, questions) {
+  console.log(diff);
+  let selQuestion;
+  for (let i = 0; i < unitQuestions.length; i++) {
+    if (questions.length === 0 && unitQuestions[i].level == diff) {
+      selQuestion = unitQuestions[i];
+      break;
+    }
+    //  console.log(questions.map(function(e) { return e.question; }).indexOf(unitQuestions[i].question)!= -1);
+    if (
+      unitQuestions[i].level == diff &&
+      questions
+        .map(function(e) {
+          return e.question;
+        })
+        .indexOf(unitQuestions[i].question) == -1
+    ) {
+      selQuestion = unitQuestions[i];
+      break;
+    }
+  }
+  return selQuestion;
+}
+function genOutput() {
+    console.log(questions[0]);
+    let ux = "";
+    for (var i = 0; i < questions.length; i++) {
+        if(questions[i] !== undefined) {
+      let str = `<p>
+        ${questions[i].question} , ${questions[i].type}, ${questions[i].level}, ${questions[i].unit}
+      </p>`;
+      ux = ux + str;
+    }
+}
+    console.log(ux);
+    document.getElementById("data").insertAdjacentHTML("beforeend", ux);
+}
